55728f45776bc1dccccf9da52ad87d9af1e47468
namenaro <nanenaro@gmail.com>
2023-05-02 10:10:06 +0300

    hdghdgff

--- w_eval/TEST.py
+++ w_eval/TEST.py
@@ -4,17 +4,21 @@ from test_w_eval_1px import test_went_around_T__1, test_went_around_T__1_
 from common_utils import Point, HtmlLogger
 from picture_wrapper import Pic
 
+
 if __name__ == '__main__':
     pic = Pic()
 
-    max_went = 9
-    T = Point(x=14, y=14)
+    max_went = 16
+    T = Point(x=12, y=14)
 
     logger = HtmlLogger("t3")
     test_went_around_T__cloud_(pic=pic, radius=3, T=T, max_went=max_went, logger=logger)
 
     logger = HtmlLogger("t2")
     test_went_around_T__cloud_(pic=pic, radius=2, T=T,  max_went=max_went, logger=logger)
+
+    logger = HtmlLogger("t4")
+    test_went_around_T__cloud_(pic=pic, radius=4, T=T, max_went=max_went, logger=logger)
 
 
     test_went_around_T__1_(pic=pic, max_went=max_went, T=T)
